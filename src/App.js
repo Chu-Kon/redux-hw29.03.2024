d6c1412e866215cde20014756a686c928fe34cf0
K Chuprin <netvorenje@yandex.kz>
2024-04-11 19:15:20 +0500

    feat: добавил переключение темы со светлой на темную

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import TodoList from './components/TodoList/TodoList';
 import LoginPage from './components/LoginPage/LoginPage';
+import Header from './components/Header';
 import { useSelector } from 'react-redux';
 import '@mantine/core/styles.css'
-import { MantineProvider } from '@mantine/core'
+import { MantineProvider } from '@mantine/core';
 
 const App = () => {
   const { isAuth } = useSelector((state) => state.auth);
   return (
     <MantineProvider>
-    <Router>
-      <Routes>
-        <Route path="/registration" element={<LoginPage />} />
-        <Route path="/todo-list" element={isAuth ? <TodoList /> : <Navigate to="/registration" />} />
-      </Routes>
-    </Router>
+      <Header></Header>
+      <Router>
+        <Routes>
+          <Route path="/registration" element={<LoginPage />} />
+          <Route path="/todo-list" element={isAuth ? <TodoList /> : <Navigate to="/registration" />} />
+        </Routes>
+      </Router>
     </MantineProvider>
   );
 };
