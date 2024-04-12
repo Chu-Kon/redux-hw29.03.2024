b79ed95d78ed37e5fea182b6c8f45f449d416538
K Chuprin <netvorenje@yandex.kz>
2024-04-12 13:57:11 +0500

    fix: создал для компонента Header и его стилей отдельную папку

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import TodoList from './components/TodoList/TodoList';
 import LoginPage from './components/LoginPage/LoginPage';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import { useSelector } from 'react-redux';
 import '@mantine/core/styles.css'
 import { MantineProvider } from '@mantine/core';
